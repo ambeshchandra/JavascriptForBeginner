f5e03a4752bd96bed22ea3e79cc8f39051fc2887
ambeshchandra <ambeshchandra@gmail.com>
2023-09-14 15:39:21 +0530

    Use of Fetch

--- Index.js
+++ Index.js
@@ -3,3 +3,10 @@ let name = 'Hello Console';
 name="Test";
 
 console.log(name);
+
+let p = fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m")
+p.then((value)=>{
+    return value.json()
+}).then((value)=>{
+    console.log(value)
+})
