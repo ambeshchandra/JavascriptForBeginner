354457bf1932ccd3729337fa73a4005c08dc0c36
ambeshchandra <ambeshchandra@gmail.com>
2023-09-15 11:35:14 +0530

    Weather Detail using API

---
+++ WeatherAPI.js
@@ -0,0 +1,29 @@
+
+const options={
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '50534b77bfmsh93927d1a2694b95p1cd585jsna5d3df30bc88',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+      }
+};
+
+let p = fetch("https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Patna", options)
+p.then((value)=>{
+    return value.json()
+}).then((value)=>{
+    console.log(value)
+
+    cloud_pct.innerHTML = value.cloud_pct
+    temp.innerHTML = value.temp
+    feels_like.innerHTML = value.feels_like
+    humidity.innerHTML = value.humidity
+    min_temp.innerHTML = value.min_temp
+    max_temp.innerHTML = value.max_temp
+    wind_speed.innerHTML = value.wind_speed
+    wind_degrees.innerHTML = value.wind_degrees
+    sunrise.innerHTML = value.sunrise
+    sunset.innerHTML = value.sunset
+   
+
+    
+})
